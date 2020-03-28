c185b3b8846e94813074d2664853ab626f9b22b1
Son NX <nguyenxuanson22299@gmail.com>
2020-03-28 22:37:25 +0700

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/images/logo.png">
-    <GuessComponent msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+    
+        <img alt="Vue logo" src="./assets/images/logo.png" />
+    
+        <GuessComponent msg="Welcome to Your Vue.js App" />
+    
+    </div>
 </template>
 
 <script>
-import { GuessComponent } from './modules/guess/components';
+import { GuessComponent } from "./modules/guess/components";
 
 export default {
-  name: 'App',
-  components: {
-    GuessComponent
-  }
-}
+    name: "App",
+    components: {
+        GuessComponent
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
