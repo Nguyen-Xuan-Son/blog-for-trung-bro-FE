dac55c4aff6151461006f5c2c09f0a8dfc285295
Son NX <nguyenxuanson22299@gmail.com>
2020-04-05 10:57:31 +0700

    .

---
+++ src/modules/admin/components/index.js
@@ -0,0 +1,7 @@
+import AdminComponent from './admin';
+import LoginComponent from './admin';
+
+export default {
+    AdminComponent,
+    LoginComponent
+}
---
+++ src/modules/admin/components/login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Login</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+    },
+    methods: {
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
